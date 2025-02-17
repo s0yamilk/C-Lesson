ac319dacca9a482578466b00596e4c3b61d791c7
s0yamilk <452336713@qq.com>
2025-02-17 10:58:53 +0800

    11

--- Lesson3变量的本质/obj/Debug/net9.0/Lesson3变量的本质.AssemblyInfo.cs
+++ Lesson3变量的本质/obj/Debug/net9.0/Lesson3变量的本质.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson3变量的本质")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d4855dd6e1bbbda62c70e1ee4bbcfda8197453a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0f716e4c2e24cb5010024eae6ad3d6431386ec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson3变量的本质")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson3变量的本质")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
