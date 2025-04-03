2e3a4e0e5f294a8d848e59f30ab142cb5939d103
s0yamilk <452336713@qq.com>
2025-04-03 18:06:25 +0800

    11

--- Lesson3变量的本质/Program.cs
+++ Lesson3变量的本质/Program.cs
@@ -40,9 +40,22 @@ using System.Threading.Tasks;
                 Console.WriteLine("ushort 所占的字节数为："+ushortSize);
                 int ulongSize = sizeof(ulong);
                 Console.WriteLine("ulong 所占的字节数为："+ulongSize);
+                Console.WriteLine("---------------------");
+                Console.WriteLine("@@总结就是数字串位数越多 占用的内存也就越大");
+                Console.WriteLine("string因为是多个字符 是可变的内容 所以不能用sizeof方法来看占多少内存@@");
                 Console.ReadKey();
+                #endregion
 
-                
+                #region 知识点二 变量的本质
+                ///变量的本质是2进制--计算机中所有的数据本质都是一堆0和1
+                ///实际上是电信号，所以0和1分别表示开和关两种状态
+                ///存储单位最小单位是bit 只能表示0和1
+                ///为了方便数据表示 有了byte的单位，是由8个bit组成的
+                ///****一个字节为8位********
+                ///1byte = 00000000 就是8个0和1的排列组合
+                ///
+
+                    
                 #endregion
             }
         }
